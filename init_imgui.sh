59551544936fec1ff8b895b5c8743bf5d1097ec9
anshmendiratta <anshmendiratta2023@gmail.com>
2025-12-07 02:47:58 -0500

    [docs] fix init sh

--- init_imgui.sh
+++ init_imgui.sh
@@ -8,7 +8,7 @@ git clone https://gitlab.com/L-4/odin-imgui
 python3 build.py # Build all backends, even though some may be unnecessary.
 
 # Create identical directory structure to README.md.
-shared_dir=$(odin root)+"shared/" 
+shared_dir=$(odin root)"shared/" 
 mkdir $(shared_dir)/dear_imgui
 mkdir $(shared_dir)/dear_imgui/gl
 mkdir $(shared_dir)/dear_imgui/glfw
